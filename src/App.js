46098ac8c1099f7214e2948194ead6ff61229296
adoumadje <adoumadjecaleb@gmail.com>
2022-06-12 01:41:38 +0300

    3h06min

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart, Checkout } from './components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { commerce } from './libs/Commerce';
 
@@ -8,6 +9,8 @@ import { commerce } from './libs/Commerce';
 const App = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -18,20 +21,69 @@ const App = () => {
     setCart(await commerce.cart.retrieve());
   }
 
+  const handleAddToCart = async (productId, quantity) => {
+    const {cart} = await commerce.cart.add(productId, quantity);
+    setCart(cart);
+  }
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const {cart} = await commerce.cart.update(productId, quantity);
+    setCart(cart);
+  }
+
+  const handleRemoveFromCart = async (productId) => {
+    const {cart} = await commerce.cart.remove(productId);
+    setCart(cart);
+  }
+
+  const handleEmptyCart = async () => {
+    const {cart} = await commerce.cart.empty();
+    setCart(cart);
+  }
+
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+
+    setCart(newCart);
+  }
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+      setOrder(incomingOrder);
+      refreshCart();
+    } catch(error) {
+      setErrorMessage(error.data.error.message);
+    }
+  }
+
   useEffect(() => {
     fetchProducts();
     fetchCart();
   }, []);
 
-
-  console.log(cart);
-
-
   return (
-    <div>
-      <Navbar />
-      <Products products={products} />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Routes>
+          <Route path='/' element={<Products products={products} onAddToCart={handleAddToCart} />} />
+          <Route 
+            path='/cart' 
+            element={
+              <Cart 
+                cart={cart} 
+                onUpdateCartQty={handleUpdateCartQty}
+                onRemoveFromCart={handleRemoveFromCart}
+                onEmptyCart={handleEmptyCart}
+              />
+            } 
+          />
+          <Route path='/checkout' element={<Checkout cart={cart} />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
